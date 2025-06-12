1e5562ad07f1c322ae57796c26d267ccc02f506f
Dzulani <128301376+dzulanimonyayi@users.noreply.github.com>
2025-06-12 09:04:57 +0200

    init commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,28 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BaseChartDirective} from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from '@angular/forms';
+import {NgOptimizedImage} from '@angular/common';
+import { OverviewComponent } from './overview/overview.component';
+
 
 @NgModule({
   declarations: [
-    App
+    App,
+    LoginComponent,
+    OverviewComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgOptimizedImage,
+    BaseChartDirective
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
